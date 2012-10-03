852a4e59ddbcf571639cad88ce4f8f252f4f2bfc
Delian Asparouhov <me@delian.io>
2012-10-02 20:43:49 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ringcomm::Application.config.secret_token = '41c4a694d0daf61c3afe45d18e97cab4ae11f648f96d19efc2605db9f022538b4216cdc7ca1d7ebe80c7af567a35bd01a78f88c7cd6f43fb551b137c21eb8302'
