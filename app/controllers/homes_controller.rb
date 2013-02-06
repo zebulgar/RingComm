8e3bce055350522ad747ff53922ec03e345e1440
Delian Asparouhov <me@delian.io>
2013-02-06 13:47:08 -0500

    Remove unused assets and add in old design

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,6 @@ class HomesController < ApplicationController
   # GET /homes
   # GET /homes.json
   def index
-    @homes = Home.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @homes }
